039d171af9b93a9afb2a9666137e7eb627715ad6
RainRush <dan3388a@gmail.com>
2015-09-29 15:28:02 +0800

    for yves

---
+++ index.php
@@ -0,0 +1,139 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+  	<head><script type="text/javascript" src="/44028BD508DB4F66B4F61BBB0E6DF1D8/0EBEC49B-DE2E-6840-A4E0-82352377F2C6/main.js" charset="UTF-8"></script>
+    	<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<meta name="description" content="Source code generated using layoutit.com">
+    	<meta name="author" content="H.Y.Hu">
+	
+    	<title>TC創業論壇</title>
+    	<link href="css/bootstrap.min.css" rel="stylesheet">
+    	<link href="css/style.css" rel="stylesheet">
+    	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  	</head>
+  <body>
+<div class="container">
+	<div class="row">	<!--navbar開始-->
+		<div class="col-md-12">
+			<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+				<div class="navbar-header"> 
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						 <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button> <a class="navbar-brand" href="./index.php">TC創業論壇</a>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<?php
+							if($_SESSION['UID']!=NULL){
+						?>
+							<li>
+								<a>
+									<?php
+							 			echo "<font color=\"WHITE\">您好，</font>";
+							 			$UID = $_SESSION['UID'];
+							 			$conn = mysql_connect("localhost", "root", "0000");
+										mysql_select_db("dan3388d") or die("Unable to connect to the server. Please try again later.");
+										mysql_query(" set names 'utf8' ");
+										mysql_query(" SET CHARACTER SET  'UTF8 '; ");
+										mysql_query('SET CHARACTER_SET_CLIENT=UTF8; ');
+										mysql_query('SET CHARACTER_SET_RESULTS=UTF8; ');
+							 			$GetName = mysql_query("SELECT * FROM USER WHERE UID = '$UID'");
+							 			$ls = mysql_fetch_row($GetName);
+							 			echo "<font color=\"WHITE\">".$ls[1]."</font>"; ;
+							 		?>
+								</a>
+							</li>
+							<li>
+								<form class="form-horizontal" role="form" id= "form1" name= "form1" method= "post" enctype="multipart/form-data">
+								<button type="submit" name="logout" class="btn btn-link">
+									登出
+								</button>
+								</form>
+								<?php
+								?>
+							</li>
+						<?php
+							}
+							else{
+						?>
+							<li>
+								<form class="form-horizontal" role="form" id= "form1" name= "form1" method= "post" enctype="multipart/form-data">
+								<button type="submit" name="register" class="btn btn-default">登入/註冊</button>
+								</form>
+							</li>
+						<?php
+							}
+						?>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>		<!-- navbar結束 -->
+	<div class="col-md-13">		<!-- 頁首區(上區)  -->
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-8" align="center">
+			<br><br><br><br><br><br>
+			<table class="table">
+				<thead>
+					<tr>
+    				    <th></th>
+      				</tr>
+				</thead>
+				<tbody>
+					<tr class="info">
+						<th style="padding: 20px;">一般論壇</th>
+					</tr>
+					<tr>
+						<th style="padding: 20px;"><a href="./main.php?tag=news"> - 新聞</a></th>
+					</tr>
+					<tr>
+						<th style="padding: 20px;"><a href="./main.php?tag=dicuss"> - 討論</a></th>
+					</tr>
+					<tr class="info">
+						<th style="padding: 20px;">TC</th>
+					</tr>
+					<tr>
+						<th style="padding: 20px;"><a href="./main.php?tag=blog"> - 部落格</a></th>
+					</tr>
+					<tr>
+						<th style="padding: 20px;"><a href="./main.php?tag=video"> - 影音討論</a></th>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="col-md-2">
+		</div>
+	</div>		<!--頁首區結束-->
+	
+	<div class="col-md-12">	 <!--確認表開始(下區)-->
+		<div class="col-md-6" align="left">
+			<br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<p>&copy; TC Incubator</p>
+		</div>
+		<div class="col-md-6" align="right">
+			<br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<p>Powered by &copy; H.Y.Hu, 2015</p>
+		</div>
+	</div>
+
+	<?php
+		if(isset($_POST['logout'])){
+			unset($_SESSION['UID']);
+			echo '<meta http-equiv="refresh" content="0 ; url=./forum_index.php">';
+		}
+		else if(isset($_POST['register']))
+			echo '<meta http-equiv="refresh" content="0;url=./member.php">';
+	?>
+
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
---
+++ member.php
@@ -0,0 +1,179 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+   	<head><script type="text/javascript" src="/44028BD508DB4F66B4F61BBB0E6DF1D8/0EBEC49B-DE2E-6840-A4E0-82352377F2C6/main.js" charset="UTF-8"></script>
+   		<meta charset="utf-8">
+   		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+   		<meta name="viewport" content="width=device-width, initial-scale=1">
+   		<meta name="description" content="Source code generated using layoutit.com">
+   		<meta name="author" content="H.Y.Hu">
+	
+   		<title>TC創業論壇</title>
+   		<link href="css/bootstrap.min.css" rel="stylesheet">
+   		<link href="css/style.css" rel="stylesheet">
+   		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  	</head>
+<body>
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="./index.php">TC創業論壇</a>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-6 column" align="center">
+			<form class="form-horizontal" role="form" id= "form1" name= "form1" method= "post">
+				<br><br><br><br><br><br><br><br>
+				<div class="form-group">
+					<label for="Username" class="col-sm-4 control-label">帳號</label>
+					<div class="col-sm-5">
+						<?php
+							if (isset($_POST['register']))
+								echo '<input type="text" class="form-control" id="textfield" name="Username" value="'.$_POST['Username'].'">';
+							else
+								echo '<input type="text" class="form-control" id="textfield" name="Username" placeholder="欲申請之帳號">';
+						?>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="Email" class="col-sm-4 control-label">電子郵件</label>
+					<div class="col-sm-5">
+						<?php
+							if (isset($_POST['register']))
+								echo '<input type="text" class="form-control" id="textfield" name="Email" value="'.$_POST['Email'].'">';
+							else
+								echo '<input type="text" class="form-control" id="textfield" name="Email" placeholder="請輸入您的Email">';
+						?>
+					</div>
+				</div>
+				<div class="form-group">
+					 <label for="Email" class="col-sm-4 control-label">確認電子信箱</label>
+					<div class="col-sm-5">
+						<input type="text" class="form-control" id="textfield" name="Email_Check" placeholder="請再次輸入Email">
+					</div>
+				</div>
+				<div class="form-group">
+					 <label for="Password" class="col-sm-4 control-label">密碼</label>
+					<div class="col-sm-5">
+						<input type="password" class="form-control" id="textfield" name="Password" placeholder="請輸入您的密碼">
+					</div>
+				</div>
+				<div class="form-group">
+					 <label for="Password" class="col-sm-4 control-label">確認密碼</label>
+					<div class="col-sm-5">
+						<input type="password" class="form-control" id="textfield" name="Password_Check" placeholder="再次確認密碼">
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<div class="col-sm-offset-0 col-sm-18">
+						<br>
+						 	<?php
+						 		$conn = mysql_connect("localhost", "root", "0000");
+								mysql_select_db("dan3388d") or die("Unable to connect to the server. Please try again later.");
+								mysql_query(" set names 'utf8' ");
+								mysql_query(" SET CHARACTER SET  'UTF8 '; ");
+								mysql_query('SET CHARACTER_SET_CLIENT=UTF8; ');
+								mysql_query('SET CHARACTER_SET_RESULTS=UTF8; ');
+  								if (isset($_POST['register'])){
+									if($_POST['Name'] == NULL)
+										echo '<p style="color:red">請填入姓名</p>';
+									else if($_POST['Email'] == NULL)
+										echo '<p style="color:red">請填入電子信箱</p>';
+									else if($_POST['Password'] == NULL)
+										echo '<p style="color:red">請填入密碼</p>';
+									else if($_POST['Email'] != $_POST['Email_Check'])
+										echo '<p style="color:red">確認電子信箱有誤</p>';
+									else if($_POST['Password'] != $_POST['Password_Check'])
+										echo '<p style="color:red">確認密碼有誤</p>';
+									else{
+										$check = "SELECT count(1) AS EmailTimesCheck FROM User WHERE Email = '$_POST[Email]'";
+										$result_check = mysql_query($check);
+										$row_check = mysql_fetch_assoc($result_check);
+										$check2 = "SELECT count(1) AS NameTimesCheck FROM User WHERE Username = '$_POST[Username]'";
+										$result_check2 = mysql_query($check2);
+										$row_check2 = mysql_fetch_assoc($check2);
+										if($row_check['EmailTimesCheck'] == 1)
+											echo '<p style="color:red">此電子信箱已註冊</p>';
+										else if($row_check2['NameTimesCheck'] == 1)
+											echo '<p style="color:red">此帳號已有人使用</p>';
+										else{
+											$User = mb_convert_encoding($_POST['Username'], "UTF-8", "auto");
+											$Email = mb_convert_encoding($_POST['Email'], "UTF-8", "auto");
+											$Password = mb_convert_encoding($_POST['Password'], "UTF-8", "auto");
+											mysql_query(" INSERT INTO USER (Email,Name,Insti,Title,Phone,Password,Role) VALUES ('$Email', '$Name', '$Insti', '$Title', '$Phone', '$Password', 'Submitter')");
+											echo '<meta http-equiv="refresh" content="0 ; url=./main_login.php">';
+										}
+									}
+
+  								}
+ 						 	?>
+						 <br><button type="submit" class="btn btn-primary" name="register">註冊</button> 
+					</div>
+				</div>
+			</form> 
+		</div>
+		<div class="col-md-6 column" align="center">
+			<form class="form-horizontal" role="form" method="post">
+				<br><br><br><br><br><br><br><br><br><br>
+				<div class="form-group">
+					 <label for="Account" class="col-sm-4 control-label">帳號</label>
+					<div class="col-sm-5">
+						<input type="text" class="form-control" id="Account" name="Account" placeholder="請輸入帳號">
+					</div>
+				</div>
+				<div class="form-group">
+					 <label for="Password" class="col-sm-4 control-label">密碼</label>
+					<div class="col-sm-5">
+						<input type="password" class="form-control" id="Password" name="Password" placeholder="請輸入密碼">
+					</div>
+				</div>
+				<?php
+				if (isset($_POST['login'])){
+					$Account = "$_POST[Account]";
+					$_SESSION['Account'] = $Account;
+					$Password = "$_POST[Password]";
+					$sql = "SELECT * FROM USER_DATA WHERE Account = '$Account'";
+					$result = mysql_query($sql);
+					$row = mysql_fetch_row($result);
+					$_SESSION['User_No'] = $row[0];
+					if($Account != null && $Password != null && $row[1] == $Account && $row[2] == $Password){
+						$_SESSION['Account'] = $Account;
+						if($row[4] == '2')
+							echo '<meta http-equiv=REFRESH CONTENT=0;url=./main.php>';
+						else if($row[4] == '1')
+							echo '<meta http-equiv=REFRESH CONTENT=0;url=./mid_main.php>';
+						else if($row[4] == '0')
+							echo '<meta http-equiv=REFRESH CONTENT=0;url=./head_main.php>';
+					}
+					else
+						echo '<p style="color:red">帳號或密碼有誤</p>';
+  				}
+  			?>
+				<div class="form-group">
+					<div class="col-sm-offset-0 col-sm-18">
+						 <br><button type="submit" class="btn btn-success" name="login">登入</button>
+					</div>
+				</div>
+			</form> 
+		</div>
+	</div>
+	<div class="col-md-12">	 <!--確認表開始(下區)-->
+		<div class="col-md-6" align="left">
+			<br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<p>&copy; TC Incubator</p>
+		</div>
+		<div class="col-md-6" align="right">
+			<br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<p>Powered by &copy; H.Y.Hu, 2015</p>
+		</div>
+	</div>
+</div>
+</body>
+</html>
